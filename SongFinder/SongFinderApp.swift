6b8986d85b7efd557146037fab51e7d4c204c4ff
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-04-20 10:26:35 -0400

    Initial Commit

---
+++ SongFinder/SongFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongFinderApp.swift
+//  SongFinder
+//
+//  Created by Quin Taylor on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct SongFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SongDetailView()
+        }
+    }
+}
